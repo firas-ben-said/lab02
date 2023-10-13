2170334ed5c94fc4d04b31f2ad7f3121b2ee0e71
pitacod <121180203+pitacod@users.noreply.github.com>
2023-10-13 10:03:10 +0100

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+if (!isset($_SESSION['user']))
+    header("location:/login.php");
+// user role must be admin, chek for it!
+
+
+require "../connexion.php";
+$req = $db->prepare("SELECT * FROM `users`");
+$req->execute();
+$users = $req->fetchAll();
+
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include "../header.php" ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-8 m-auto">
+                <table class="table table-striped table-hover">
+                    <tr>
+                        <th>username</th>
+                        <th>password</th>
+                        <th>email</th>
+                        <th>action</th>
+
+                    </tr>
+                    <?php foreach ($users as $user) { ?>
+                        <tr>
+                            <td></td>
+                            <td>></td>
+                            <td></td>
+                            <td>
+                                <a class="btn btn-danger btn-sm" href="/admin/delete.php">delete</a>
+                            </td>
+
+
+                        </tr>
+                    <?php } ?>
+
+                </table>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
